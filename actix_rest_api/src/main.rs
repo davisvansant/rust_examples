142956b7104d5a1fdb9612aa67b0bc2e9e9a2940
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-03-26 18:59:46 -0700

    initial tests for actix_rest_api

--- actix_rest_api/src/main.rs
+++ actix_rest_api/src/main.rs
@@ -1,14 +1,12 @@
-use actix_web::{web, App, Responder, HttpServer, get, HttpResponse};
+use actix_web::{web, App, HttpServer, HttpResponse, HttpRequest};
 use actix_web::middleware::Logger;
 use env_logger::Env;
 
-#[get("/albums")]
-async fn albums() -> impl Responder {
+async fn albums(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("Albums!")
 }
 
-#[get("/eps")]
-async fn eps() -> impl Responder {
+async fn eps(_req: HttpRequest) -> HttpResponse {
     HttpResponse::Ok().body("EPs!")
 }
 
@@ -19,10 +17,47 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(Logger::default())
-            .service(albums)
-            .service(eps)
+            .route("/albums", web::get().to(albums))
+            .route("/eps", web::get().to(eps))
     })
     .bind("0.0.0.0:8080")?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::test;
+    use actix_web::http::StatusCode;
+
+    #[actix_rt::test]
+    async fn unit_handler_albums() {
+        let req = test::TestRequest::default().to_http_request();
+        let resp = albums(req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn unit_handler_eps() {
+        let req = test::TestRequest::default().to_http_request();
+        let resp = eps(req).await;
+        assert_eq!(resp.status(), StatusCode::OK);
+    }
+
+    #[actix_rt::test]
+    async fn integration_handler_albums() {
+        let mut app = test::init_service(App::new().route("/albums", web::get().to(albums))).await;
+        let req = test::TestRequest::get().uri("/albums").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
+
+    #[actix_rt::test]
+    async fn integration_handler_eps() {
+        let mut app = test::init_service(App::new().route("/eps", web::get().to(albums))).await;
+        let req = test::TestRequest::get().uri("/eps").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
 }
