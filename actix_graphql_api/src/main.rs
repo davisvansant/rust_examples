989c384ca908ecd2477337c22916ae46f4d36a44
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-03-31 14:38:03 -0700

    initial web setup, with graphiql and playground

--- actix_graphql_api/src/main.rs
+++ actix_graphql_api/src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::*;
+use actix_web::middleware::Logger;
+use env_logger::Env;
+use juniper::*;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::playground::playground_source;
+
+async fn graphiql() -> HttpResponse {
+    let html = graphiql_source("/graphiql");
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+async fn playground() -> HttpResponse {
+    let html = playground_source("/playground");
+
+    HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(html)
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    env_logger::from_env(Env::default().default_filter_or("info")).init();
+
+    HttpServer::new(|| {
+        App::new()
+            .wrap(Logger::default())
+            .route("/graphiql", web::get().to(graphiql))
+            .route("/playground", web::get().to(playground))
+    })
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
